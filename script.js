b099caa9d6a3746675633547c58a22b83d636f91
gabriellemia <gabrielle@mia.com>
2023-03-22 15:48:36 +0000

    Started to create the javascript. Naming functions needed etc.

---
+++ script.js
@@ -0,0 +1,37 @@
+const numberButtons = document.querySelectorAll('.number');
+const operationButtons = document.querySelectorAll('.operation');
+const equalsButton = document.querySelector('.equals');
+const clearButton = document.querySelector('.clear');
+const deleteButton = document.querySelector('.delete');
+const screenPrevious = document.querySelector('.previous-input');
+const screenCurrent = document.querySelector('.current-input');
+const addButton = document.querySelector('#add');
+const subtractButton = document.querySelector('#subtract');
+const multiplyButton = document.querySelector('#multiply');
+const divideButton = document.querySelector('#divide');
+const calculator = document.querySelector('.calculator');
+
+
+function chooseOperation(operation) {
+
+}
+
+function chooseNumber(number) {
+
+}
+
+function chooseClear() {
+
+}
+
+function chooseDelete() {
+
+}
+
+function calculate() {
+
+}
+
+function setDisplay () {
+
+}
